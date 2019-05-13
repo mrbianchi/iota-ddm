0da4e3429438fac913daf15014c2708d65cd7978
mrbianchi <marcelorbianchi96@gmail.com>
2019-05-13 20:12:18 -0300

    growing

--- DDM.js
+++ DDM.js
@@ -4,7 +4,6 @@ const rpchub = 'localhost:50051';// rpchub (https://github.com/iotaledger/rpchub
 const flashSeed = 'POLUYZLOHULUXCKSZYENYILPHLSONCQNADFYXDUWFXZUHPEWQGKKXKENCEFFHOGJWGYEEAYWFEAYFB9E9';// Flash Channel Seed (https://github.com/iotaledger/iota.flash.js/)
 
 // DDM Dependencies
-const IOTA = require("iota.lib.js");
 const transfer = require("./lib/transfer");
 const multisig = require("./lib/multisig");
 const Helpers = require("./functions");
@@ -19,7 +18,10 @@ const express = require('express'),
     util = require('util'),
     https = require('https'),
     fs = require('fs'),
-    prompt = require('prompt');
+    prompt = require('prompt'),
+    iotaAreaCodes = require('@iota/area-codes'),
+    { composeAPI } = require('@iota/core'),
+    { asciiToTrytes } = require('@iota/converter');
 
 // Functions
 function loadRpchub(){//return gRPC hub client
@@ -110,7 +112,7 @@ function loadHttpListener() {
     app.listen(80, 'localhost');
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
-    app.post('/devices/:name/stream',(req,res)=>{
+    app.post('/devices/:name/stream',function(req,res) {
         console.log(data)
         console.log(req.params.name);
         res.send(req.body);
@@ -118,7 +120,7 @@ function loadHttpListener() {
 }
 
 function main() {
-    var iota = new IOTA({ provider: IOTANode });
+    iota = new composeAPI({ provider: IOTANode });
     var client = loadRpchub();
     data = loadData();
     loadWallet(client);
@@ -132,18 +134,129 @@ function main() {
     menu();
 }
 
+function saveDB() {
+    fs.writeFile('DDM.db.json', JSON.stringify(data), 'utf8', function(err) {
+        if(err) console.error("Error saving DB",err);
+    });
+}
+
+function advertiseDevice(device,id) {
+    if(typeof device == "undefined") {
+        return prompt.get({
+            properties: {
+              deviceId: {
+                message: 'Invalid device ID',
+                required: true,
+                conform: function (value) {
+                    return typeof data.devices[parseInt(value)] != 'undefined'
+                }
+              }
+          }
+        }, function (err, input) {
+            advertiseDevice(data.devices[parseInt(input.deviceId)],parseInt(input.deviceId));
+        });
+    }
+    console.log("Advertising device");
+    var IACs = getOuterIACs(device.IAC);
+    var message = asciiToTrytes(JSON.stringify(getDeviceInfo(device,"advertising")));
+    var transfers = IACs.map(function(tag) {
+        return {
+            tag,
+            message,
+            address: "TESTING9999999999999999999999999999999999999999999999999999999999999999999999999C",
+            value: 0
+        };
+    });
+    
+    iota.prepareTransfers("999999999999999999999999999999999999999999999999999999999999999999999999999999999",transfers)
+    .then(function(trytes) {
+        data.devices[id].advertisementBundle = trytes;
+        saveDB();
+        return iota.sendTrytes(trytes, 3, 14)
+    })
+
+    .then(function(transactionObjects) {
+        data.devices[id].advertisementHash = transactionObjects[0].bundle;
+        console.log("Advertised device: "+transactionObjects[0].bundle)
+        saveDB();
+        menu();
+    })
+
+    .catch(console.log)
+}
+
+function getDeviceInfo(device,scope) {
+    switch(scope) {
+        case "advertising":
+            return {
+                name:device.name,
+                description:device.description,
+                IAC:device.IAC,
+                category:device.category,
+                price:device.price,
+                deviceFrequency:device.deviceFrequency,
+                structure:device.structure,
+                sensors:device.sensors
+            }
+        break;
+        case "listDevicesFn":
+        return {
+            name:device.name,
+            description:device.description,
+            IAC:device.IAC,
+            category:device.category,
+            price:device.price,
+            deviceFrequency:device.deviceFrequency,
+            structure:device.structure,
+            sensors:device.sensors,
+            advertisementHash:device.advertisementHash
+        }
+        break;
+        default:
+            return device;
+        break;
+    }
+}
+
+// Returns outer IACs (includes the original IAC)
+function getOuterIACs(IAC) {
+    var IACs = [IAC];
+    var error = false;
+    do{
+        try {
+            IACs.push(iotaAreaCodes.decreasePrecision(IACs.slice(-1)[0]));
+        }catch(e) {
+            error = e;
+        }
+    }while(!error);
+    return IACs;
+}
+
+function getNodeInfo() {
+    iota.getNodeInfo()
+    .then(info => { 
+        console.log(info);
+        menu();
+     })
+    .catch(error => {
+        console.error(`Request error: ${error.message}`)
+    })
+}
+
 function menu(){
     console.log("Menu");
     var options = {
-        "Get Node Info":console.log,
+        "Get Node Info":getNodeInfo,
         "Device's list":listDevices,
         "Add Device":addDevice,
+        "Advertise Device": advertiseDevice,
+        //"Convert lat,lon to IAC code": convertLL2IAC,
         "Subscriptions's list":console.log,
-        "Add device":console.log,
         "Subscribe to device":console.log,
+        "Find devices with IAC":console.log,
         "Close DDM": process.exit
     };
-    Object.keys(options).forEach((option,n)=>{
+    Object.keys(options).forEach(function(option,n) {
         console.log(`${n}. ${option}`);
     });
     prompt.get(['menu'], function (err, result) {
@@ -153,17 +266,70 @@ function menu(){
 
 function listDevices() {
     console.log("Device List:");
-    console.log(data.devices);
+    data.devices.forEach(device => {
+        console.log(getDeviceInfo(device,"listDevicesFn"))
+    });
     menu();
 }
 
 function addDevice() {
-    console.log("Enter sensor data:")
-    prompt.get(['name','description','IAC','category','price','deviceFrequency','structure','sensors'], function (err, result) {
-        console.log(result);
-        menu();
-    });
-    //menu();
-}
+    var schema = {
+        properties: {
+            IAC: {
+                description: 'IOTA IAC Code',
+                message: 'Invalid IAC code',
+                required: true,
+                conform: function (value) {
+                    return iotaAreaCodes.isValid(value);
+                }
+            },
+            name: {
+                description: "*Device Name",
+                required: true
+            },
+            description: {
+                description: "Device Description",
+            },
+            category: {
+                description: "Sensors Category",
+            },
+            price: {
+                required: true,
+                description: "*Stream Price (i)"
+            },
+            deviceFrequency: {
+                description: "Frequency Data Stream (kHz)"
+            },
+            structure: {
+                required: true,
+                description: "*Device Output Structure"
+            },
+            sensors: {
+                description: "Device's Sensors Quantity",
+            }
+        }
+    };      
+    console.log("Add new device:")
+    prompt.get(schema, function (err, device) {
+        var id = data.devices.push(device)-1;
+        saveDB();
+        console.log(`Device ${id} added! Want to advertise it? (y=Yes , n=No)`);
+        
+        prompt.get({
+            properties: {
+              answer: {
+                message: 'Invalid answer (y=Yes , n=No)',
+                required: true,
+                conform: function (value) {
+                    return value == "y" || value == "n";
+                }
+              }
+          }
+        }, function (err, answer) {
+            if(answer == "y") advertiseDevice(device,id);
+            else menu();
+        });
 
+    });
+}
 main();
